21e891a74313e91c6be0b79d51cc9f742b116552
Wolfrand <wolfrand90@gmail.com>
2025-04-08 20:47:00 -0300

    introduzido na tela informações de seguidores do usuário

--- src/js/objects/screen.js
+++ src/js/objects/screen.js
@@ -6,6 +6,9 @@ const screen = {
                                         <div class="data">
                                             <h1>${user.name ?? "Não possui nome cadastrado"}</h1>
                                             <p>${user.bio ?? "Não possui bio cadastrada"}</p>
+                                            <br>
+                                            <p>👥 Seguidores: ${user.followers}</p>
+                                            <p>👤 Seguindo: ${user.following}</p>
                                         </div>
                                     </div>`
 
